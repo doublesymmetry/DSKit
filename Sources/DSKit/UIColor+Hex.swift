a1560bde6552d9c4a972a295d2f162228532d52f
Jules <juliane@doublesymmetry.com>
2022-08-18 16:45:25 +0200

    add UIColor+Hex file

---
+++ Sources/DSKit/UIColor+Hex.swift
@@ -0,0 +1,60 @@
+//
+//  UIColor+Hex.swift
+//  DSKit
+//
+//  Created by David Chavez on 11.01.22.
+//
+
+import UIKit
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let red, green, blue, alpha: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    red = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    green = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    blue = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    alpha = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: red, green: green, blue: blue, alpha: alpha)
+                    return
+                }
+            } else if hexColor.count == 6 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    red = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+                    green = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+                    blue = CGFloat(hexNumber & 0x0000ff) / 255
+
+                    self.init(red: red, green: green, blue: blue, alpha: 1.0)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+
+    var hexValue: String {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+
+        getRed(&red, green: &green, blue: &blue, alpha: nil)
+
+        let rgb: Int = (Int)(red * 255) << 16 | (Int)(green * 255) << 8 | (Int)(blue * 255) << 0
+
+        return String(format: "#%06x", rgb)
+    }
+}
