affe8fe02f2c0c91b2e1770d7dfabccfb9835678
Jules <juliane@doublesymmetry.com>
2022-08-22 15:36:45 +0200

    add @available

--- Sources/DSKit/UIControl+addAction.swift
+++ Sources/DSKit/UIControl+addAction.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UIControl {
+    @available(iOS 14, *)
     func addAction(for controlEvents: UIControl.Event = .touchUpInside, _ closure: @escaping() -> Void) {
         addAction(UIAction { _ in closure() }, for: controlEvents)
     }
