f8a8eae76142f869f497107660bad4fcc4ff0435
Jules <juliane@doublesymmetry.com>
2022-08-22 11:42:04 +0200

    change font to San Francisco

--- Sources/DSKit/String+HtmlAttributedString.swift
+++ Sources/DSKit/String+HtmlAttributedString.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 extension String {
-    func toHtmlAttributedString(fontFamily: String = "Lato", size: CGFloat, color: UIColor) -> NSAttributedString? {
+    func toHtmlAttributedString(fontFamily: String = "San Francisco", size: CGFloat, color: UIColor) -> NSAttributedString? {
         do {
             let htmlCSSString = "<style>" +
                 "html *" +
