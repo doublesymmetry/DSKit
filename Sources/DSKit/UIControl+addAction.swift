8e4c3f7ac3bc8a280ee40ec66be3ed5a1b63803a
JS-arch <63039848+JS-arch@users.noreply.github.com>
2022-08-22 17:21:19 +0200

    add UIControl+addAction (#14)

---
+++ Sources/DSKit/UIControl+addAction.swift
@@ -0,0 +1,15 @@
+//
+//  UIControl+addAction.swift
+//  DSKit
+//
+//  Created by Fabian Sulzbacher on 13.01.22.
+//
+
+import UIKit
+
+extension UIControl {
+    @available(iOS 14, *)
+    func addAction(for controlEvents: UIControl.Event = .touchUpInside, _ closure: @escaping() -> Void) {
+        addAction(UIAction { _ in closure() }, for: controlEvents)
+    }
+}
