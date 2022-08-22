ef592bd8819662da3b5d9c407c964efa4c3f45f5
JS-arch <63039848+JS-arch@users.noreply.github.com>
2022-08-22 14:31:12 +0200

    add GradientLayer file (#11)

---
+++ Sources/DSKit/GradientLayer.swift
@@ -0,0 +1,42 @@
+//
+//  GradientLayer.swift
+//  DSKit
+//
+//  Created by Fabian
+//
+
+import Foundation
+import UIKit
+
+class GradientLayer: CAGradientLayer {
+
+    struct Configuration {
+        let gradientColors: [GradientColor]
+        let startPoint: CGPoint
+        let endPoint: CGPoint
+
+        struct GradientColor {
+            let color: CGColor
+            let location: NSNumber
+        }
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    // Fixes an issue, where the view debugger was not working anymore, when invoked from a view which containt a GradientLayer.
+    // https://stackoverflow.com/questions/31892986/why-does-cabasicanimation-try-to-initialize-another-instance-of-my-custom-calaye/36017699
+
+    override init(layer: Any) {
+        super.init(layer: layer)
+    }
+
+    init(configuration: Configuration, frame: CGRect) {
+        super.init(layer: CAGradientLayer.self)
+        colors = configuration.gradientColors.map { $0.color }
+        locations = configuration.gradientColors.map { $0.location }
+        startPoint = configuration.startPoint
+        endPoint = configuration.endPoint
+        self.frame = frame
+    }
+}
