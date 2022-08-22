b4169440a1d97db33d75ee43e86433a31d23545f
Jules <juliane@doublesymmetry.com>
2022-08-22 12:11:48 +0200

    add GradientColorConfiguration as enum

--- Sources/DSKit/GradientLayer.swift
+++ Sources/DSKit/GradientLayer.swift
@@ -8,6 +8,39 @@
 import Foundation
 import UIKit
 
+struct GradientColor {
+    let color: CGColor
+    let location: NSNumber
+}
+
+enum GradientColorConfiguration {
+    case custom([GradientColor], CGPoint, CGPoint)
+    var colors: [CGColor] {
+        switch self {
+        case .custom(let gradientColors, _, _):
+            return gradientColors.map { $0.color }
+        }
+    }
+    var locations: [NSNumber] {
+        switch self {
+        case .custom(let gradientColors, _, _):
+            return gradientColors.map { $0.location }
+        }
+    }
+    var startPoint: CGPoint {
+        switch self {
+        case .custom(_, let startPoint, _):
+            return startPoint
+        }
+    }
+    var endPoint: CGPoint {
+        switch self {
+        case .custom(_, _, let endPoint):
+            return endPoint
+        }
+    }
+}
+
 class GradientLayer: CAGradientLayer {
 
     @available(*, unavailable)
@@ -15,6 +48,7 @@ class GradientLayer: CAGradientLayer {
 
     // Fixes an issue, where the view debugger was not working anymore, when invoked from a view which containt a GradientLayer.
     // https://stackoverflow.com/questions/31892986/why-does-cabasicanimation-try-to-initialize-another-instance-of-my-custom-calaye/36017699
+
     override init(layer: Any) {
         super.init(layer: layer)
     }
