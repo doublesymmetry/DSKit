214a30be9a426bb1833451fe4b8957a41853f1fc
dcvz <david@dcvz.io>
2024-01-02 23:48:31 +0100

    feature(collection): Add collection methods

---
+++ Sources/DSKit/Swift/Collection+Emptiness.swift
@@ -0,0 +1,30 @@
+//
+//  Collection+Emptiness.swift
+//  DSKit
+//
+//  Created by Milen Pivchev on 11/23/21.
+//
+
+extension Collection {
+    /// A Boolean value indicating whether the collection is **NOT** empty.
+    ///
+    /// When you need to check whether your collection is not empty, use the
+    /// `isNotEmpty` property instead of checking that the `count` property is
+    /// not equal to zero. For collections that don't conform to
+    /// `RandomAccessCollection`, accessing the `count` property iterates
+    /// through the elements of the collection.
+    ///
+    ///     let horseName = "Silver"
+    ///     if horseName.isNotEmpty {
+    ///         print("Hi ho, \(horseName)!")
+    ///     } else {
+    ///         print("My horse has no name.")
+    ///     }
+    ///     // Prints "Hi ho, Silver!"
+    ///
+    var isNotEmpty: Bool {
+        get {
+            return !isEmpty
+        }
+    }
+}
